76c9d31199e6752fa7c9e9ccff6ded14ba12b9c9
whitywhity0487-gif <whitywhity0487@gmail.com>
2026-06-22 14:54:48 +0530

    Update AdminAssetsManagement.jsx

--- src/Pages/admin/AdminAssetsManagement.jsx
+++ src/Pages/admin/AdminAssetsManagement.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const API_BASE_URL = "http://localhost:5000/api/employeeassets";
+const API_BASE_URL = "https://uaw-backend.vercel.app/api/employeeassets";
 
 const AdminAssetsManagement = () => {
   const [assets, setAssets] = useState([]);
